35e7ce288fd31cc67ef6429074741cd70f0a9781
Steven Nunez <steven.nunez@gmail.com>
2015-08-17 14:37:21 -0400

    Code So Far

--- models/student.rb
+++ models/student.rb
@@ -1,7 +1,18 @@
 class Student
+  @@db = SQLite3::Database.new('db/students_store.db')
+  @@db.results_as_hash = true
+
   attr_reader :id, :name, :super_power
   def self.new_from_row(row)
     new(row["id"], row["name"], row["super_power"])
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+    results = @@db.execute(sql)
+    results.map {|row| self.new_from_row(row) }
   end
 
   def initialize(id, name, super_power)
--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,4 +1,3 @@
-
 describe Student do
   it "creates an instance from a sqlite row" do
     row = {
@@ -14,4 +13,17 @@ describe Student do
     expect(student.name).to eq("George")
     expect(student.super_power).to eq("perfect pitch")
   end
+
+  it "returns all students in the database" do
+    # Static finder methods to wrap commonly used
+    # SQL queries and return Active Record objects
+    students = Student.all
+    expect(students.first).to be_a(Student)
+    expect(students.first.name).to eq("George")
+  end
 end
+
+# Student.find(7)
+# Student.find_by(:name, "George")
+# s = Student.new
+# s.save
